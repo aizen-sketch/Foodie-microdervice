03b8c0043c9fb63a6bef54cd994bbf7537439cdf
Partha Hait <partha@Parthas-MacBook-Air.local>
2025-11-12 01:23:06 +0530

    menu service role control finalize

--- menu-service/src/main/java/com/example/demo/controllers/MenuController.java
+++ menu-service/src/main/java/com/example/demo/controllers/MenuController.java
@@ -1,13 +1,9 @@
 package com.example.demo.controllers;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import com.example.demo.Entity.menuItems;
 import com.example.demo.dto.MenuItemDto;
 import com.example.demo.repository.menuRepository;
@@ -22,38 +18,63 @@ public class MenuController {
 	@Autowired
 	private MenuItemService menuService;
 	
+	public boolean adminAccess(
+	        @RequestHeader("X-User-Id") String userIdFromToken,
+	        @RequestHeader("X-User-Role") String userRoleFromToken) {
+
+		if("ADMIN".equalsIgnoreCase(userRoleFromToken))
+		return true;
+		return false;
+		
+	}
+	
+	public ResponseEntity<?> forbiddenAccess(){
+		return new ResponseEntity<>("Only admin can access",HttpStatus.FORBIDDEN); 
+	}
 
 	@GetMapping("/all")
 	public ResponseEntity<?> getAllMenuItems(
 	        @RequestHeader("X-User-Id") String userIdFromToken,
-	        @RequestHeader("X-User-Role") String userRoleFromToken) {
-
-	    if (!"ADMIN".equalsIgnoreCase(userRoleFromToken)) {
-	        return ResponseEntity.status(HttpStatus.FORBIDDEN)
-	                .body("Access denied: Only ADMIN users can view all menu items.");
-	    }
-
+	        @RequestHeader("X-User-Role") String userRoleFromToken
+	        ) {
 	    return ResponseEntity.ok(menuService.getAllItems());
 	}
 	@GetMapping("/{id}")
-	public MenuItemDto getAllMenuById(@PathVariable Integer id) {
+	public ResponseEntity<?> getAllMenuById(@PathVariable Integer id,
+			@RequestHeader("X-User-Id") String userIdFromToken,
+	        @RequestHeader("X-User-Role") String userRoleFromToken
+	        ) {
 	     menuItems item = repo.findById(id).orElse(null) ;
-		 return new MenuItemDto(item.getMenuId(), item.getName(), item.getPrice());
+	     return ResponseEntity.ok(new MenuItemDto(item.getMenuId(), item.getName(), item.getPrice()));
 	}
 	@PostMapping
-	public MenuItemDto createMenuItem(@RequestBody MenuItemDto item) {
-	    return menuService.addItem(item);
+	public ResponseEntity<?>  createMenuItem(@RequestBody MenuItemDto item,
+			@RequestHeader("X-User-Id") String userIdFromToken,
+	        @RequestHeader("X-User-Role") String userRoleFromToken
+			) {
+		if(adminAccess(userIdFromToken, userRoleFromToken)==true)
+	    return ResponseEntity.ok(menuService.addItem(item));
+		return forbiddenAccess();
 	}
 
-	@PutMapping("/{id}")
-	public MenuItemDto updateMenuItem(@PathVariable Integer id, @RequestBody MenuItemDto item) {
-	    return menuService.updateItem(id, item);
+	@PostMapping("update/{id}")
+	public ResponseEntity<?> updateMenuItem(@PathVariable Integer id,@RequestBody MenuItemDto item,
+			@RequestHeader("X-User-Id") String userIdFromToken,
+	        @RequestHeader("X-User-Role") String userRoleFromToken
+			) {
+		if(adminAccess(userIdFromToken, userRoleFromToken)==true)
+	    return ResponseEntity.ok(menuService.updateItem(id, item));
+		return forbiddenAccess();
 	}
 	
 
 	@DeleteMapping("/{id}")
-	public String deleteMenuItem(@PathVariable Integer id) {
-	    return menuService.deleteItem(id);
+	public ResponseEntity<?>  deleteMenuItem(@PathVariable Integer id,
+			@RequestHeader("X-User-Id") String userIdFromToken,
+	        @RequestHeader("X-User-Role") String userRoleFromToken) {
+		if(adminAccess(userIdFromToken, userRoleFromToken)==true)
+			return ResponseEntity.ok(menuService.deleteItem(id));
+		return forbiddenAccess();
 	}
 
 }
